314e5746c9211d8b42d5c6c321eda234918d7838
Duks31 <chidubemjan31@gmail.com>
2024-02-16 08:32:46 +0100

    added new workflow

--- core/db.py
+++ core/db.py
@@ -1,23 +1,21 @@
-from sqlalchemy import create_engine, Table, MetaData, Column, Integer, String, DateTime
+import os
+from sqlalchemy import create_engine, Column, Integer, String, DateTime
 from sqlalchemy.orm import mapper, sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 from py_code.news_api_data import news_data
 from datetime import datetime
 
-engine = create_engine('sqlite:///news_data.db', echo=True)
+db_path = 'sqlite:///news_data.db'
+
+if os.path.exists(db_path):
+    os.remove(db_path)
+    
+engine = create_engine(db_path, echo=True)
 
 Base = declarative_base()
 
 class NewsData(Base):
     __tablename__ = 'news_data'
-    def __init__(self, title, description, content, url, publishedAt, source_name, source_id):
-        self.title = title
-        self.description = description
-        self.content = content
-        self.url = url
-        self.publishedAt = publishedAt
-        self.source_name = source_name
-        self.source_id = source_id
 
     id = Column(Integer, primary_key=True)
     title = Column(String)
@@ -29,27 +27,37 @@ class NewsData(Base):
     source_id = Column(String)
 
 Base.metadata.create_all(engine)
+
 Session = sessionmaker(bind=engine)
 
 def insert_news_data(data):
     if data is None:
         print("No data to insert")
+        return 
+
     session = Session()
-    for article in data['articles']:
-        publication_at = datetime.fromisoformat(article['publishedAt'].rstrip('Z'))
-        news_data = NewsData(
-            title = article['title'],
-            description = article['description'],
-            content = article['content'],
-            url = article['url'],
-            publishedAt = publication_at,
-            source_name = article['source']['name'],
-            source_id = article['source']['id']
-        )
-        session.add(news_data)
-    session.commit()
-    session.close()
+    try:
+        for article in data['articles']:
+            publication_at = datetime.fromisoformat(article['publishedAt'].rstrip('Z'))
+            news_data = NewsData(
+                title = article['title'],
+                description = article['description'],
+                content = article['content'],
+                url = article['url'],
+                publishedAt = publication_at,
+                source_name = article['source']['name'],
+                source_id = article['source']['id']
+            )
+            session.add(news_data)
+        session.commit()
+    except Exception as e:
+        print(f"Error while inserting data: {e}")
+        # session.rollback()
+    finally:
+        session.close()
 
 if __name__ == "__main__":
-    news_data = news_data()
-    insert_news_data(news_data)
+    retrieved_news_data = news_data()
+    print(retrieved_news_data)
+    insert_news_data(retrieved_news_data)   
+
